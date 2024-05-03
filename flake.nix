fc21a056a565e703bbcbc798d43eaf3d3fe3c0f0
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-05-03 10:38:28 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    description = "A Nix-flake-based development environment";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self , nixpkgs ,... }: let
+        system = "x86_64-linux";
+    in {
+        devShells."${system}".default = let
+            pkgs = import nixpkgs {
+                inherit system;
+            };
+        in pkgs.mkShell {
+            packages = with pkgs; [
+                libcxx
+                clang
+                clang-tools
+                gdb
+                valgrind
+                premake5
+            ];
+
+            shellHook = ''
+                exec zsh
+                '';
+        };
+    };
+}
