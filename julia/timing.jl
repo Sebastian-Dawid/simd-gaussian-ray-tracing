3acfc596296020c7f09d32bb468a36a3d01638bd
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-05-24 09:41:13 +0200

    timing script plots transmittance time (simd vs. seq) now.

--- julia/timing.jl
+++ julia/timing.jl
@@ -23,7 +23,14 @@ function load_data()
     plot!(count, t, label="fast exp")
     t = Vector(data[:, 4])
     plot!(count, t, label="std::exp")
-    return plt_erf, plt_exp
+    
+    data = CSV.File(data_path*"../csv/timing_transmittance.csv") |> DataFrame
+    count = Vector(data[:, 1])
+    t = Vector(data[:, 2])
+    plt_trans = plot(count, t, dpi=300, label="simd", xlabel="count", ylabel=L"$\mu s$", title="Time per pixel w.r.t. the number of gaussians\nin the scene (exp)")
+    t = Vector(data[:, 3])
+    plot!(count, t, label="seq")
+    return plt_erf, plt_exp, plt_trans
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
@@ -31,7 +38,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
     if !isdir(img_dir)
         mkdir(img_dir)
     end
-    plt_erf, plt_exp = load_data()
+    plt_erf, plt_exp, plt_trans = load_data()
     savefig(plt_erf, img_dir*"/timing_erf.png")
     savefig(plt_exp, img_dir*"/timing_exp.png")
+    savefig(plt_trans, img_dir*"/timing_transmittance.png")
 end
