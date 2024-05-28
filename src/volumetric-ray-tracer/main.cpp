78ea1c349aea4098ca51ed5b132a7448e900ebb7
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-05-28 11:55:41 +0200

    Improved cli. projection plane positions are now precomputed

--- src/volumetric-ray-tracer/main.cpp
+++ src/volumetric-ray-tracer/main.cpp
@@ -48,9 +48,13 @@ int main(i32 argc, char **argv)
     std::vector<char*> args(argc);
     std::memcpy(args.data(), argv, sizeof(*argv) * argc);
     bool grid = false;
+    u64 idx = 0;
+    u64 filename_idx = -1;
     for (char *s : args)
     {
         if (std::strncmp(s, "--grid", 7) == 0) grid = true;
+        if (std::strncmp(s, "-o", 3) == 0) filename_idx = idx+1;
+        ++idx;
     }
 
     std::vector<gaussian_t> gaussians;
@@ -81,6 +85,7 @@ int main(i32 argc, char **argv)
     bool use_abramowitz_approx = false;
     bool use_fast_exp = false;
     bool use_simd_transmittance = false;
+    bool use_simd_pixels = false;
     if (!renderer.init(width, height, "Test")) return EXIT_FAILURE;
     renderer.custom_imgui = [&](){
         ImGui::Begin("Gaussians");
@@ -93,13 +98,28 @@ int main(i32 argc, char **argv)
         ImGui::Checkbox("erf taylor", &use_taylor_approx);
         ImGui::Checkbox("erf abramowitz", &use_abramowitz_approx);
         ImGui::Checkbox("exp fast", &use_fast_exp);
-        ImGui::Checkbox("use simd", &use_simd_transmittance);
+        ImGui::Checkbox("use simd innermost", &use_simd_transmittance);
+        ImGui::Checkbox("use simd pixels", &use_simd_pixels);
         ImGui::End();
     };
     bool running = true;
     std::thread render_thread([&](){ renderer.run(running); });
 
     u32 *image = (u32*)std::calloc(width * height, sizeof(u32));
+
+    float x[width * height];
+    float y[width * height];
+
+    for (u64 i = 0; i < height; ++i)
+    {
+        for (u64 j = 0; j < width; ++j)
+        {
+            x[i * width + j] = -1.f + j/(width/2.f);
+            y[i * width + j] = -1.f + i/(height/2.f);
+        }
+    }
+
+    u64 step = 1;
 
     while (running)
     {
@@ -118,34 +138,30 @@ int main(i32 argc, char **argv)
         if (use_fast_exp) _exp = fast_exp;
         else _exp = std::exp;
         if (use_simd_transmittance) _transmittance = simd_transmittance;
-        else _transmittance = transmittance;
+        else { _transmittance = transmittance; }
 
         auto start_time = std::chrono::system_clock::now();
         vec4f_t pt = { -1.f, -1.f, 0.f };
-        for (u64 y = 0; y < height; ++y)
+        for (u64 i = 0; i < width * height; i += step)
         {
-            for (u64 x = 0; x < width; ++x)
-            {
-                vec4f_t dir = pt - origin;
-                dir.normalize();
-                vec4f_t color = l_hat(origin, dir, gaussians);
-                u32 A = 0xFF000000; // final alpha channel is always 1
-                u32 R = (u32)(color.x * 255);
-                u32 G = (u32)(color.y * 255);
-                u32 B = (u32)(color.z * 255);
-                image[y * width + x] = A | R << 16 | G << 8 | B;
-                pt.x += 1/(width/2.f);
-                if (!running) goto cleanup;
-            }
-            pt.x = -1.f;
-            pt.y += 1/(height/2.f);
+            pt.x = x[i];
+            pt.y = y[i];
+            vec4f_t dir = pt - origin;
+            dir.normalize();
+            vec4f_t color = l_hat(origin, dir, gaussians);
+            u32 A = 0xFF000000; // final alpha channel is always 1
+            u32 R = (u32)(color.x * 255);
+            u32 G = (u32)(color.y * 255);
+            u32 B = (u32)(color.z * 255);
+            image[i] = A | R << 16 | G << 8 | B;
+            if (!running) goto cleanup;
         }
 
         auto end_time = std::chrono::system_clock::now();
         draw_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
-        if (argc >= 4)
-            stbi_write_png(argv[3], width, height, 4, image, width * 4);
+        if (filename_idx != (u64)-1)
+            stbi_write_png(argv[filename_idx], width, height, 4, image, width * 4);
         renderer.stage_image(image, width, height);
     }
 
